c518028747e71a443c8e6f377fe5c51dceded353
Nazim Lachter <nazim@blueforest.cc>
2022-12-17 19:01:35 +0100

    more servers

--- sync.ts
+++ sync.ts
@@ -66,5 +66,5 @@ for (const [language, accounts] of Object.entries(feeds)) {
 /*
 [NOTES FOR THE AUTHORS] Token acquisition process (cannot be automated yet):
   1. https://toot.aquilenet.fr/oauth/authorize?response_type=code&client_id=645xncPGFQLlgRSE-GUJKKupk732BuYNLbC0AwUnflY&redirect_uri=urn:ietf:wg:oauth:2.0:oob&scope=read:accounts+read:statuses+write:statuses
-  2. curl - X POST "https://toot.aquilenet.fr/oauth/token?grant_type=authorization_code&code=$CODE&client_id=645xncPGFQLlgRSE-GUJKKupk732BuYNLbC0AwUnflY&client_secret=$SECRET&redirect_uri=urn:ietf:wg:oauth:2.0:oob&scope=read:accounts+read:statuses+write:statuses"
+  2. curl -X POST "https://toot.aquilenet.fr/oauth/token?grant_type=authorization_code&code=$CODE&client_id=645xncPGFQLlgRSE-GUJKKupk732BuYNLbC0AwUnflY&client_secret=$SECRET&redirect_uri=urn:ietf:wg:oauth:2.0:oob&scope=read:accounts+read:statuses+write:statuses"
 */
