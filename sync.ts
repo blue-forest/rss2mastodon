01a9b67d7a8e45b65cc56a4b6b90b242e5cb7979
Nazim Lachter <nazim@blueforest.cc>
2022-12-17 20:25:51 +0100

    handle redirections + request function

--- sync.ts
+++ sync.ts
@@ -9,58 +9,89 @@ const feeds = parseYaml(await Deno.readTextFile("./feeds.yml")) as {
 
 for (const [language, accounts] of Object.entries(feeds)) {
   for (const [account, url] of Object.entries(accounts)) {
-    const [name, instance] = account.split("@")
-    const envName = `TOKEN_${`${name}_${instance}`.replace(/\./g, "_").toUpperCase()}`
-    const token = Deno.env.get(envName)
-    if (!token) {
-      console.log(`No token for ${account} from ${envName}`)
-      continue
-    }
+    try {
+      const [name, instance] = account.split("@")
 
-    // Get feed
-    const feedResponse = await fetch(url)
-    const feedXML = await feedResponse.text()
-    let feed = (await parseFeed(feedXML)).entries.map(e => ({
-      text: e.title?.value,
-      url: e.links[0].href,
-    }))
+      // Token
+      const envName = `TOKEN_${`${name}_${instance}`.replace(/\./g, "_").toUpperCase()}`
+      const token = Deno.env.get(envName)
+      if (!token) {
+        console.log(`No token for ${account} from ${envName}`)
+        continue
+      }
 
-    // Get user ID
-    const credentialsResponse = await fetch(
-      `https://${instance}/api/v1/accounts/verify_credentials`,
-      { headers: { Authorization: `Bearer ${token}` } },
-    )
-    const { id } = await credentialsResponse.json()
+      // Get feed
+      const feedResponse = await fetch(url)
+      const feedXML = await feedResponse.text()
+      let feed = (await parseFeed(feedXML)).entries.map(e => ({
+        text: e.title?.value,
+        url: e.links[0].href,
+      }))
 
-    // Get last status
-    const lastStatusResponse = await fetch(
-      `https://${instance}/api/v1/accounts/${id}/statuses?limit=1&exclude_replies=true&exclude_reblogs=true`,
-      { headers: { Authorization: `Bearer ${token}` } },
-    )
-    const [lastStatus] = await lastStatusResponse.json()
+      // Get user ID
+      const { id } = await request(
+        instance,
+        "GET",
+        `accounts/verify_credentials`,
+        token,
+      )
 
-    // Remove already posted statuses
-    if (lastStatus) {
-      const lastURLIndex = feed.findIndex(e => e.url === lastStatus.card.url)
-      if (lastURLIndex !== -1) feed = feed.slice(0, lastURLIndex)
-    }
+      // Get last status
+      const [lastStatus] = await request(
+        instance,
+        "GET",
+        `accounts/${id}/statuses?limit=1&exclude_replies=true&exclude_reblogs=true`,
+        token,
+      )
 
-    for (const entry of feed.reverse()) {
-      const response = await fetch(`https://${instance}/api/v1/statuses`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify({
-          status: `${entry.text} ${entry.url}`,
-          visibility: "public",
-          language: language,
-        }),
-      })
-      console.log(account, entry, response.status)
+      // Remove already posted statuses
+      if (lastStatus) {
+        for (let i = 0; i < feed.length; i++) {
+          const entry = feed[i]
+          if (!entry.url) continue
+          const redirectedURL = await getRedirectedURL(entry.url)
+          if (lastStatus.card.url.startsWith(redirectedURL)) {
+            feed = feed.slice(0, i)
+            break
+          }
+        }
+      }
+
+      for (const entry of feed.reverse()) {
+        const response = await request(
+          instance,
+          "POST",
+          `statuses`,
+          token,
+          JSON.stringify({
+            status: `${entry.text} ${entry.url}`,
+            visibility: "public",
+            language: language,
+          }),
+        )
+        console.log(account, entry.url, response.id)
+      }
+    } catch (error) {
+      console.error("ERROR", account, error)
     }
   }
+}
+
+async function getRedirectedURL(url: string) {
+  const response = await fetch(url, { method: "HEAD" })
+  return response.url
+}
+
+async function request(instance: string, method: string, path: string, token: string, body?: string) {
+  const response = await fetch(`https://${instance}/api/v1/${path}`, {
+    method,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body,
+  })
+  return response.json()
 }
 
 /*
